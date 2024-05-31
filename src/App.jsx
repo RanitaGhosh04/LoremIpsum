80c00a882e60eb5e297222a4620b4386f4dee944
Ranita Ghosh <ranitaghosh04@gmail.com>
2024-05-31 14:46:00 +0530

    learning hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,50 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import LoremGenerator from './component/LoremGenerator'
+import data from './component/data'
 
 function App() {
 
+ const [count, setCount] = useState(0)
+ const [text, setText] = useState([])
 
+ const handleSubmit = (e) => {
+  console.log(e);
+  e.preventDefault()
+  let amount = parseInt(count)
+
+  if(amount<=0){
+    amount = 1
+  }
+  if(amount>8){
+    amount = 8
+  }
+
+  setText(data.slice(0,amount))
+ }
   return (
     <>
- <LoremGenerator />
+ {/* <LoremGenerator /> */}
+
+ <section>
+  <h3>Genrate Lorun ipsum</h3>
+  <form onSubmit={handleSubmit}>
+    <input type="number"
+    value={count}
+    onChange={ (e)=>
+      {setCount(e.target.value)
+        // console.log(e);
+      }}
+     />
+     <button>Generate</button>
+  </form>
+  <article>
+    {
+      text.map((item,index)=>{
+        return <p key={index}>{item}</p>
+      })
+    }
+  </article>
+ </section>
     </>
   )
 }
